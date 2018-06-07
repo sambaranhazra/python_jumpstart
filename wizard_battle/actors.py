333cdbed331627cd755a47e9550e01daee7ef796
Sambaran Hazra <sambaran2010@gmail.com>
2018-06-07 06:07:39 +0530

    Adding inheritence

--- wizard_battle/actors.py
+++ wizard_battle/actors.py
@@ -1,15 +1,26 @@
 import random
 
 
-class Wizard:
+class Creature:
     def __init__(self, name, level):
         self.name = name
         self.level = level
 
+    def __repr__(self):
+        return "Creature {} of level {}".format(self.name, self.level)
+
+    def get_defensive_roll(self):
+        return random.randint(1, 12) * self.level
+
+
+class Wizard(Creature):
+    def __init__(self, name, level):
+        super().__init__(name, level)
+
     def attack(self, creature):
         print("The wizard {} attacks {}".format(self.name, creature.name))
-        my_roll = random.randint(1, 12) * self.level
-        creature_roll = random.randint(1, 12) * creature.level
+        my_roll = self.get_defensive_roll()
+        creature_roll = creature.get_defensive_roll()
         print("You roll {}...".format(my_roll))
         print("{} rolls {}...".format(creature.name, creature_roll))
         if my_roll >= creature_roll:
@@ -19,12 +30,22 @@ class Wizard:
             return False
 
 
-class Creature:
-    # level
-    # name
-    def __init__(self, name, level):
-        self.name = name
-        self.level = level
+class SmallAnimal(Creature):
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        return base_roll / 2
 
-    def __repr__(self):
-        return "Creature {} of level {}".format(self.name, self.level)
+
+class Dragon(Creature):
+    def __init__(self, name, level, scale_thickness, breathe_fire=False):
+        super().__init__(name, level)
+        self.breathe_fire = breathe_fire
+        self.scale_thickness = scale_thickness
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        fire_modifier = 5 if self.breathe_fire else 1
+        scale_modifier = self.scale_thickness / 10
+        return base_roll * fire_modifier * scale_modifier
+
+    # def breathe_fire(self):
--- wizard_battle/program.py
+++ wizard_battle/program.py
@@ -2,7 +2,7 @@ import random
 
 import time
 
-from actors import Wizard, Creature
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 def main():
@@ -19,11 +19,11 @@ def print_header():
 
 def game_loop():
     creatures = [
-        Creature("Toad", 1),
+        SmallAnimal("Toad", 1),
         Creature("Tiger", 12),
-        Creature("Bat", 3),
-        Creature("Dragon", 50),
-        Creature("Evil wizard", 1000),
+        SmallAnimal("Bat", 3),
+        Dragon("Dragon", 50, scale_thickness=20),
+        Wizard("Evil wizard", 1000),
     ]
     print(creatures)
     hero = Wizard("Gandalf", 75)
@@ -40,9 +40,11 @@ def game_loop():
                 time.sleep(5)
                 print("Wizard returns revitalized!!!")
         elif cmd == "r":
-            print("Run away")
+            print("{} ran away to take shelter.".format(hero.name))
         elif cmd == "l":
-            print("Look around")
+            print("{} looking around for creatures".format(hero.name))
+            for creature in creatures:
+                print("* {} of level {} near.".format(creature.name, creature.level))
         else:
             print("OK, exiting game.")
             break
